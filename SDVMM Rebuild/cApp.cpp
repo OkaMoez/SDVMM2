7c37b2a6536f0fa917ff705fa1690c036d61ac64
OkaMoez <43766412+OkaMoez@users.noreply.github.com>
2019-09-24 13:51:50 -0700

    Implement DEBUG macro with toggles for specific dialogue boxes

--- SDVMM Rebuild/cApp.cpp
+++ SDVMM Rebuild/cApp.cpp
@@ -1,3 +1,10 @@
+#ifdef _DEBUG 
+#define D(x) x
+#include "debugConstants.h"
+#else 
+#define D(x)
+#endif
+
 #include "cApp.h"
 
 wxIMPLEMENT_APP(cApp);
@@ -18,20 +25,30 @@ bool cApp::OnInit()
 	m_frame1->Show(true);
 	if (!StartCheck(m_frame1))
 	{
-		wxMessageDialog* m_pBox1 = new wxMessageDialog(NULL,
-			wxT("No .ini file was found."), wxT("File Not Found"),
-			wxOK, wxDefaultPosition);
-		m_pBox1->ShowModal();
-		delete m_pBox1;
+		D(
+			if (report_ini_exists) {
+				wxMessageDialog* m_pBox1 = new wxMessageDialog(NULL,
+					wxT("No .ini file was found."), wxT("INI Not Found"),
+					wxOK, wxDefaultPosition);
+				m_pBox1->ShowModal();
+				delete m_pBox1;
+			}
+			else {}
+		)
 	}
 	else
 	{
 		string myPath = m_frame1->gamepath();
-		wxMessageDialog* m_pBox1 = new wxMessageDialog(NULL,
-			myPath, wxT("Game Location"),
-			wxOK, wxDefaultPosition);
-		m_pBox1->ShowModal();
-		delete m_pBox1;
+		D(
+			if (report_game_directory) {
+				wxMessageDialog* m_pBox1 = new wxMessageDialog(NULL,
+					myPath, wxT("Game Directory"),
+					wxOK, wxDefaultPosition);
+				m_pBox1->ShowModal();
+				delete m_pBox1;
+			}
+			else {}
+		)
 	}
 
 	return true;
@@ -58,52 +75,64 @@ bool cApp::StartCheck(cMain* m_frame)
 				temp_path = p.path();
 				temp_path += "\\manifest.json";
 				
-				/*
-				wxMessageDialog* m_pBox6 = new wxMessageDialog(NULL, //test
-					(temp_path.string()), wxT("Path Check"),
-					wxOK, wxDefaultPosition);
-				m_pBox6->ShowModal();
-				delete m_pBox6;
-				*/
+				D(
+					if (report_looped_path) {
+						wxMessageDialog* m_pBox6 = new wxMessageDialog(NULL,
+							(temp_path.string()), wxT("Path Check"),
+							wxOK, wxDefaultPosition);
+						m_pBox6->ShowModal();
+						delete m_pBox6;
+					}
+					else {}
+				)
 
 				ifstream i(temp_path.c_str());
 				try {
 					json_manifest = json::parse(i); // TODO prompt user to handle trailing commas
 				}
-				catch (std::exception& e) {
-					string temp_exc = e.what();
-
-					wxMessageDialog* m_pBox2 = new wxMessageDialog(NULL, //test
-						temp_exc, wxT("Exception Report"),
-						wxOK, wxDefaultPosition);
-					m_pBox2->ShowModal();
-					delete m_pBox2;
+				catch (std::exception & e) {
+					D(
+						if (report_parse_exception) {
+							string temp_exc = e.what();
+							wxMessageDialog* m_pBox2 = new wxMessageDialog(NULL,
+								temp_exc, wxT("Exception Report"),
+								wxOK, wxDefaultPosition);
+							m_pBox2->ShowModal();
+							delete m_pBox2;
+						}
+						else {}
+					)
 				}
-
 				string temp_v = "";
 				try {
 					if (json_manifest.at("Version").is_object()) { throw std::exception("obj not str"); }
 					else
 					json_manifest.at("Version").get_to(temp_v);
 
-					/*
-					wxMessageDialog* m_pBox2 = new wxMessageDialog(NULL, //test
-						temp_v, wxT("File Check"),
-						wxOK, wxDefaultPosition);
-					m_pBox2->ShowModal();
-					delete m_pBox2;
-					*/
+					D(
+						if (report_got_version) {
+							wxMessageDialog* m_pBox2 = new wxMessageDialog(NULL,
+								("got version: " + temp_v), wxT("File Check"),
+								wxOK, wxDefaultPosition);
+							m_pBox2->ShowModal();
+							delete m_pBox2;
+						}
+						else {}
+					)
 				}
 				catch (std::exception& e) {
 					string temp_exc = e.what();
 
-					
-					wxMessageDialog* m_pBox2 = new wxMessageDialog(NULL, //test
-						temp_exc, wxT("Exception Report"),
-						wxOK, wxDefaultPosition);
-					m_pBox2->ShowModal();
-					delete m_pBox2;
-					
+					D(
+						if (report_version_exception) {
+							wxMessageDialog* m_pBox2 = new wxMessageDialog(NULL,
+								temp_exc, wxT("Exception Report"),
+								wxOK, wxDefaultPosition);
+							m_pBox2->ShowModal();
+							delete m_pBox2;
+						}
+						else {}
+					)
 
 					int temp_v1 = NULL;
 					int temp_v2 = NULL;
@@ -120,16 +149,21 @@ bool cApp::StartCheck(cMain* m_frame)
 
 				if (fileExists(temp_path.string()))
 				{
-					string temp_msg1 = "";
-					string temp_msg2 = "";
-					json_manifest["Name"].get_to(temp_msg1);
-					json_manifest["Version"].get_to(temp_msg2);
-					wxMessageDialog* m_pBox2 = new wxMessageDialog(NULL, //test
-						(temp_msg1 + " exists, " + temp_msg2),
-						wxT("File Check"), wxOK, wxDefaultPosition);
-					m_pBox2->ShowModal();
-					delete m_pBox2;
-
+					D(
+						if (report_parsed_mod_data) {
+							string temp_msg1 = "";
+							string temp_msg2 = "";
+							json_manifest["Name"].get_to(temp_msg1);
+							json_manifest["Version"].get_to(temp_msg2);
+							wxMessageDialog* m_pBox2 = new wxMessageDialog(NULL,
+								(temp_msg1 + " exists, " + temp_msg2),
+								wxT("File Check"), wxOK, wxDefaultPosition);
+							m_pBox2->ShowModal();
+							delete m_pBox2;
+						}
+						else {}
+					)
+					
 					//cMod* test_mod = new cMod(json_manifest);
 					//delete test_mod;
 				}
---
+++ SDVMM Rebuild/debugConstants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Debug Toggles
+// cApp
+bool report_ini_exists = true;
+bool report_game_directory = true;
+bool report_filepath = true;
+bool report_json_name = true;
+bool report_looped_path = true;
+bool report_parse_exception = true;
+bool report_got_version = true;
+bool report_version_exception = true;
+bool report_parsed_mod_data = true;
