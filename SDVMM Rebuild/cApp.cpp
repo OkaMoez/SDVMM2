d6f06b99f9fbe318ae4713bd39aab2233aee9f8a
OkaMoez <43766412+OkaMoez@users.noreply.github.com>
2019-09-24 11:38:16 -0700

    Fix unhandled exceptions when accessing "Version" in .json files

--- SDVMM Rebuild/cApp.cpp
+++ SDVMM Rebuild/cApp.cpp
@@ -44,27 +44,27 @@ bool cApp::StartCheck(cMain* m_frame)
 	{
 		if (FILE * iniFile = fopen(file_name.c_str(), "r")) {
 			m_frame->set_gamepath(getDirectory(iniFile));
+			fclose(iniFile);
+
 			fs::path temp_dir = (m_frame->gamepath());
 			temp_dir += "\\Mods";
 			fs::path temp_path = temp_dir;
 			fs::path temp_stop = temp_dir +="\\";
-			//temp_path += "\\AutoGate\\manifest.json";
 			
 			json json_manifest;
 			for (auto& p : fs::directory_iterator(temp_dir))
 			{
 				
 				temp_path = p.path();
-				/*
+				temp_path += "\\manifest.json";
+				
 				wxMessageDialog* m_pBox6 = new wxMessageDialog(NULL, //test
 					(temp_path.string()), wxT("Path Check"),
 					wxOK, wxDefaultPosition);
 				m_pBox6->ShowModal();
 				delete m_pBox6;
-				*/
-				temp_path += "\\manifest.json";
 
-				ifstream i(temp_path);
+				ifstream i(temp_path.c_str());
 				try {
 					json_manifest = json::parse(i);
 				}
@@ -72,17 +72,28 @@ bool cApp::StartCheck(cMain* m_frame)
 					std::cerr << e.what() << std::endl;
 				}
 
-				if (json_manifest.find("MajorVersion") != json_manifest.end())
-				{
+				string temp_v = "";
+				try {
+					json_manifest.at("Version").get_to(temp_v);
+				}
+				catch (std::exception & e) {
+					std::cerr << e.what() << std::endl;
+
 					wxMessageDialog* m_pBox2 = new wxMessageDialog(NULL, //test
 						wxT("Gotcha"), wxT("File Check"),
 						wxOK, wxDefaultPosition);
 					m_pBox2->ShowModal();
 					delete m_pBox2;
 
-					string temp_v = string(json_manifest["Version"]["MajorVersion"]) +
-						+"." + string(json_manifest["Version"]["MinorVersion"]) +
-						+"." + string(json_manifest["Version"]["PatchVersion"]);
+					int temp_v1 = NULL;
+					int temp_v2 = NULL;
+					int temp_v3 = NULL;
+					json_manifest["Version"]["MajorVersion"].get_to(temp_v1);
+					json_manifest["Version"]["MinorVersion"].get_to(temp_v2);
+					json_manifest["Version"]["PatchVersion"].get_to(temp_v3);
+					temp_v = std::to_string(temp_v1) 
+						+ "." + std::to_string(temp_v2)
+						+ "." + std::to_string(temp_v3);
 					json_manifest.erase("Version");
 					json_manifest["Version"] = temp_v;
 				}
@@ -99,7 +110,6 @@ bool cApp::StartCheck(cMain* m_frame)
 					//delete test_mod;
 				}
 			}
-			fclose(iniFile);
 			return true;
 		}
 		else
