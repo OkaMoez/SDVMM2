36571f41190f80653df3485b0f74c2b092b5ab57
OkaMoez <43766412+OkaMoez@users.noreply.github.com>
2019-09-24 13:06:55 -0700

    Prevent errors around version number format + parsing

--- SDVMM Rebuild/cMod.cpp
+++ SDVMM Rebuild/cMod.cpp
@@ -13,9 +13,13 @@ cMod::cMod(string name, string auth, string ver, string desc, string uId)
 }
 cMod::cMod(json manifest)
 {
-	cMod(string(manifest["Name"]), string(manifest["Author"]),
-		string(manifest["Version"]), string(manifest["Description"]),
-		string(manifest["UniqueID"]));
+	string tmp1, tmp2, tmp3, tmp4, tmp5;
+	manifest["Name"].get_to(tmp1);
+	manifest["Author"].get_to(tmp2);
+	manifest["Version"].get_to(tmp3);
+	manifest["Description"].get_to(tmp4);
+	manifest["UniqueID"].get_to(tmp5);
+	cMod(tmp1, tmp2, tmp3, tmp4, tmp5);
 }
 
 cMod::~cMod()
@@ -23,7 +27,7 @@ cMod::~cMod()
 
 }
 
-void cMod::set_intVersion()
+void cMod::set_intVersion() // TODO Handle Short Version Nums w/ minimal technical debt
 {
 	string temp_version = modVersion_;
 	size_t temp_pos = temp_version.find('.');
@@ -31,8 +35,15 @@ void cMod::set_intVersion()
 	temp_version = temp_version.erase(0, temp_pos+1);
 	temp_pos = temp_version.find('.');
 	minorVersion_ = stoi(temp_version.substr(0, temp_pos));
-	temp_version = temp_version.erase(0, temp_pos + 1);
-	patchVersion_ = stoi(temp_version.substr(0, temp_version.size()));
+	if (temp_pos = temp_version.size()) // Short version number "x.x"? kinda dumb
+	{
+		patchVersion_ = 0;
+	}
+	else // regular length x.x.x
+	{
+		temp_version = temp_version.erase(0, temp_pos + 1);
+		patchVersion_ = stoi(temp_version.substr(0, temp_version.size()));
+	}
 }
 
 void cMod::set_modName(string name)
