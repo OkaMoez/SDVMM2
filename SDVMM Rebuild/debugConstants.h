a223b40abdac35432753b99459a82d5918a2ebff
OkaMoez <43766412+OkaMoez@users.noreply.github.com>
2019-09-24 19:10:20 -0700

    Fix Macro build errors

--- SDVMM Rebuild/debugConstants.h
+++ SDVMM Rebuild/debugConstants.h
@@ -1,14 +1,41 @@
-#pragma once
+// Define R for reports, and Q for quiet
 
-// Debug Toggles
+//#define myLOUD
+#ifdef myLOUD
+#define myREPORT(x) x
+#define myQUIET(y)
+#else
+#define myREPORT(x)
+#define myQUIET(y) y
+#endif
+
+
+// Individual Debug Toggles
 // cApp
-const bool report_ini_exists = false;
-const bool report_game_directory = false;
-const bool report_filepath = false;
-const bool report_json_name = false;
-const bool report_looped_path = false;
-const bool report_parse_exception = true;
-const bool report_got_version = false;
-const bool report_version_exception = true;
-const bool report_parsed_mod_data = true;
-const bool report_mod_object_data = true;
+const bool report_mode = true;
+
+myREPORT(
+	const bool report_ini_exists = false;
+	const bool report_game_directory = false;
+	const bool report_filepath = false;
+	const bool report_json_name = false;
+	const bool report_looped_path = false;
+	const bool report_parse_exception = true;
+	const bool report_got_version = false;
+	const bool report_version_exception = true;
+	const bool report_parsed_mod_data = true;
+	const bool report_mod_object_data = true;
+)
+
+myQUIET(
+	const bool report_ini_exists = false;
+	const bool report_game_directory = false;
+	const bool report_filepath = false;
+	const bool report_json_name = false;
+	const bool report_looped_path = false;
+	const bool report_parse_exception = false;
+	const bool report_got_version = false;
+	const bool report_version_exception = false;
+	const bool report_parsed_mod_data = false;
+	const bool report_mod_object_data = false;
+)
