e5de79409c8916743a26ada1995fb15ca6705f67
OkaMoez <43766412+OkaMoez@users.noreply.github.com>
2019-09-24 17:48:20 -0700

    Consolidate enumerator code and delete old file

--- SDVMM Rebuild/wxDirectoriesEnumerator.h
+++ SDVMM Rebuild/wxDirectoriesEnumerator.h
@@ -1,10 +1,15 @@
 #pragma once
+
+#include <string>
 #include <wx/dir.h>
 
-class wxDirectoriesEnumerator : public wxDirTraverser {
+// Found at:
+// https://stackoverflow.com/questions/20655601/wxwidgets-write-all-folders-into-a-listbox/20667534
+
+class myDirectoriesEnumerator : public wxDirTraverser {
 public:
 	wxArrayString* dirs;
-	wxDirectoriesEnumerator(wxArrayString* dirs_);
-	virtual wxDirTraverseResult OnFile(const wxString& filename);
-	virtual wxDirTraverseResult OnDir(const wxString& dirname);
+	myDirectoriesEnumerator(wxArrayString* dirs_) { dirs = dirs_; }
+	virtual wxDirTraverseResult OnFile(const wxString& filename) { return wxDIR_CONTINUE; }
+	virtual wxDirTraverseResult OnDir(const wxString& dirname) { dirs->Add(dirname); return wxDIR_IGNORE; }
 };
